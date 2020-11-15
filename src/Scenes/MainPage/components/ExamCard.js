bba62e67435aa001bcc944c9f0cdce9410a18fab
sajjad <sajjad.yazdanparast.tehrani@gmail.com>
2020-11-16 01:09:45 +0330

    designed exam card, need to be validated

--- src/Scenes/MainPage/components/ExamCard.js
+++ src/Scenes/MainPage/components/ExamCard.js
@@ -1,12 +1,74 @@
-import React from 'react'
-
-function ExamCard() {
-    return (
-        <div>
-            
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import sample_image from "../../../assets/static/logo512.png";
+import { orange } from "@material-ui/core/colors";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 300,
+    minWidth: 250,
+    position: "relative",
+    margin: theme.spacing(2),
+    backgroundColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: theme.typography.fontWeightBold,
+    float: "center",
+  },
+  avatar: {
+    height: theme.spacing(7),
+    width: theme.spacing(7),
+    position: "absolute",
+    top : theme.spacing(4.6),
+    marginRight: theme.spacing(3),
+  },
+  except_avatar: {
+    position: "relative",
+    left: theme.spacing(10),
+  },
+  fixed_writings: {
+    float: "left",
+    marginRight: theme.spacing(2),
+  },
+  props_content: {
+    fontWeight: theme.typography.fontWeightBold,
+  },
+  status_content: {
+    fontWeight: theme.typography.fontWeightBold,
+  }
+}));
+function ExamCard(props) {
+  const classes = useStyles();
+ 
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Avatar
+          className={classes.avatar}
+          alt="exam image"
+          src={sample_image}
+        />
+        <div className={classes.except_avatar}>
+          <Typography className={classes.title}>{props.ExamName}</Typography>
+          <Typography className={classes.fixed_writings}>تاریخ</Typography>
+          <Typography className={classes.props_content}>
+            {props.ExamDate}
+          </Typography>
+          <Typography className={classes.fixed_writings}>زمان</Typography>
+          <Typography className={classes.props_content}>
+            {props.ExamTime}
+          </Typography>
+          <Typography className={classes.props_content}>
+            {props.ExamStatus}
+          </Typography>
         </div>
-    )
+      </CardContent>
+    </Card>
+  );
 }
 
-
-export default ExamCard
+export default ExamCard;
--- src/Scenes/MainPage/index.tsx
+++ src/Scenes/MainPage/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import ExamCard from "./components/ExamCard";
 
 const index = () => {
-  return <div>this is a main page</div>;
+  return (
+  <div>
+    <p>this is a main page</p>
+
+    <ExamCard ExamName="طراحی نرم‌افزار شی‌گرا" ExamDate = "۱۳۹۹/۸/۷" ExamTime ="۱۳:۳۰" ExamStatus="هنوز برگزار نشده‌است"/>
+    
+
+  </div>
+  
+  );
 };
 
 export default index;

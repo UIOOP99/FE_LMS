eb6acd13db88bdd4f7d51f935e3ad052b463a36a
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2020-11-19 01:15:27 +0330

    implement navbar

---
+++ src/Scenes/MainPage/components/Navbar.tsx
@@ -0,0 +1,118 @@
+import React, { ReactElement } from 'react';
+import { Avatar, Button, Grid, IconButton, makeStyles } from '@material-ui/core';
+import { redirect } from 'services/navigation';
+import { Home, Settings } from '@material-ui/icons';
+
+interface ILink {
+  title: string,
+  path: string,
+  IconElement?: ReactElement,
+}
+
+const useStyles = makeStyles((theme) => ({
+  navbarBanner: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: theme.spacing(10),
+    backgroundColor: theme.palette.grey['300'],
+  },
+  navbar: {
+    minHeight: theme.spacing(5),
+    padding: theme.spacing(0, 1),
+    backgroundColor: theme.palette.grey['100'],
+  },
+  linkContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  avatarContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  settingButtonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  },
+  settingButton: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
+
+const navbarLinks: ILink[] = [
+  {title: 'خانه', path: '/', IconElement: <Home />},
+  {title: 'گلستان', path: 'https://golestan.ui.ac.ir/'},
+  {title: 'آزمون', path: '.'},
+  {title: 'تغذیه', path: '.'},
+  {title: 'کتابخانه', path: '.'},
+];
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  const handleNavigateToLink = (path: string) => redirect(path);
+
+  const handleSettingClick = () => {
+    return;
+  };
+  
+  const handleAvatarClick = () => {
+    return;
+  };
+
+  const renderLink = (link: ILink) => {
+    const IconElement = link.IconElement || null;
+    return (
+    <Grid className={classes.linkContainer} key={link.title} item>
+      <Button 
+        disableElevation
+        size="large"
+        variant="text"
+        {...(IconElement && {startIcon: IconElement})}
+        onClick={() => handleNavigateToLink(link.path)}
+        >
+        {link.title}
+      </Button>
+    </Grid>);
+  };
+
+  const renderNavbarBannerImage = () => {
+    return (
+      <Grid className={classes.navbarBanner} item xs={12} >BANNER</Grid>
+    );
+  };
+  const renderNavbar = () => {
+    return (
+      <Grid className={classes.navbar} item container xs={12}>
+        {navbarLinks.map(renderLink)}
+
+        <Grid item xs />
+        
+        <Grid className={classes.settingButtonContainer} item>
+          <IconButton onClick={handleSettingClick}>
+            <Settings className={classes.settingButton}/>
+          </IconButton>
+        </Grid>
+        <Grid className={classes.avatarContainer} item>
+          <IconButton onClick={handleAvatarClick}>
+            <Avatar className={classes.avatar} src="avatar"/>
+          </IconButton>
+        </Grid>
+      </Grid>
+    );
+  };
+  
+  return (
+    <Grid container>
+      {renderNavbarBannerImage()}
+      {renderNavbar()}
+    </Grid>
+  );
+};
+
+export default Navbar;
--- src/Scenes/MainPage/index.tsx
+++ src/Scenes/MainPage/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MessageCard from "./components/MessageCard";
+import Navbar from "./components/Navbar";
 
 const index = () => {
   const attachedFiles = [
@@ -12,16 +13,19 @@ const index = () => {
   ];
   const message = 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.';
 
-  return (<div>
-    <MessageCard 
-      userFullName="امیررضا اسماعیلی" 
-      classRoomName="مهندسی نت 2" 
-      message={message} 
-      attachedFiles={attachedFiles} 
-      messageDate={new Date()}
-      userAnswers={userAnswers}
-    />
-  </div>);
+  return (
+    <div>
+      <Navbar />
+      <MessageCard 
+        userFullName="امیررضا اسماعیلی" 
+        classRoomName="مهندسی نت 2" 
+        message={message} 
+        attachedFiles={attachedFiles} 
+        messageDate={new Date()}
+        userAnswers={userAnswers}
+      />
+    </div>
+  );
 };
 
 export default index;

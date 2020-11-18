dcd1dcdf9c8ae488bd50587f992a0bf33f295e3d
sajjad <sajjad.yazdanparast.tehrani@gmail.com>
2020-11-18 19:18:40 +0330

    complete ExamCard.js :)

--- src/Scenes/MainPage/index.tsx
+++ src/Scenes/MainPage/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ExamCard from "./components/ExamCard";
+import ExamCard from "../../Scenes/components/ExamCard";
 
 const index = () => {
   return (
   <div>
     <p>this is a main page</p>
 
-    <ExamCard ExamName="طراحی نرم‌افزار شی‌گرا" ExamDate = "۱۳۹۹/۸/۷" ExamTime ="۱۳:۳۰" ExamStatus="هنوز برگزار نشده‌است"/>
+    <ExamCard ExamName="من غلام قمرم غیر قمر هیچ مگو پیش من جز سخن شمع و شکر هیچ مگو سخن رنج مگو جز سخن گنج مگو ور از این بی خبری رنج مبر هیچ مگو" ExamDate = "۱۳۹۹/۸/۷" ExamTime ="۱۳:۳۰" ExamStatus="هنوز برگزار نشده‌است"/>
     
 
   </div>
--- src/Scenes/components/ExamCard.js
+++ src/Scenes/components/ExamCard.js
@@ -4,41 +4,59 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
-import sample_image from "../../../assets/static/logo512.png";
-import { orange } from "@material-ui/core/colors";
+import sample_image from "../../assets/static/logo512.png";
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 300,
-    minWidth: 250,
     position: "relative",
     margin: theme.spacing(2),
     backgroundColor: "gray",
   },
-  title: {
-    fontSize: 18,
-    fontWeight: theme.typography.fontWeightBold,
-    float: "center",
-  },
+  
+  avatar_container : {
+    width : "20%" ,
+    height : "100%" , 
+    position: "absolute",
+    top : 0 ,
+    left : 0 
+  }, 
   avatar: {
     height: theme.spacing(7),
     width: theme.spacing(7),
-    position: "absolute",
-    top : theme.spacing(4.6),
-    marginRight: theme.spacing(3),
+    top : "50%" ,
+    transform : "translate(0,-50%)"
   },
   except_avatar: {
-    position: "relative",
-    left: theme.spacing(10),
+    width: "80%" ,
+    height : "100%",
+    float : "right",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: theme.typography.fontWeightBold,
+    width: "100%",
+    wordWrap: "break-word",
+    textAlign : "center"
   },
   fixed_writings: {
     float: "left",
-    marginRight: theme.spacing(2),
+    textAlign : "center" ,
+    width : "20%",
+    margin : "1% auto"
   },
   props_content: {
+    width : "80%",
+    float : "right",
+    textAlign : "center" ,
     fontWeight: theme.typography.fontWeightBold,
+    margin : "1% auto"
   },
-  status_content: {
-    fontWeight: theme.typography.fontWeightBold,
+  status :{
+      width : "80%",
+      float : "center",
+      textAlign : "center" ,
+      fontWeight: theme.typography.fontWeightBold,
+      margin : "1% auto"
   }
 }));
 function ExamCard(props) {
@@ -47,11 +65,13 @@ function ExamCard(props) {
   return (
     <Card className={classes.root}>
       <CardContent>
+        <div className={classes.avatar_container}>
         <Avatar
           className={classes.avatar}
           alt="exam image"
           src={sample_image}
         />
+        </div>
         <div className={classes.except_avatar}>
           <Typography className={classes.title}>{props.ExamName}</Typography>
           <Typography className={classes.fixed_writings}>تاریخ</Typography>
@@ -62,7 +82,7 @@ function ExamCard(props) {
           <Typography className={classes.props_content}>
             {props.ExamTime}
           </Typography>
-          <Typography className={classes.props_content}>
+          <Typography className={classes.status}>
             {props.ExamStatus}
           </Typography>
         </div>

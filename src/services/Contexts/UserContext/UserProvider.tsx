a304ca1cf3f9480795a9cfe76a7a14e00a87dc95
alisajadih <alisajadih@gmail.com>
2020-12-07 20:15:56 +0330

    swr api fetcher

--- src/services/Contexts/UserContext/UserProvider.tsx
+++ src/services/Contexts/UserContext/UserProvider.tsx
@@ -1,15 +1,21 @@
 import React, { useReducer } from "react";
 import { userReducer } from "./UserReducer";
-import { IUserState } from "./models";
-import { userContext, userDispatchContext } from ".";
+import { EUserActionTypes, IUserState } from "./models";
+import { userContext, userDispatchContext, useUserDispatch } from ".";
+import { axiosInstance } from "services/axios/axios";
+import { LSService } from "services/LocalStorage/localStorage";
 
 const initial: IUserState = {
   username: "",
   isAuth: false,
   rule: "company",
 };
+
+
+
 const UserProvider: React.FC = ({ children }) => {
   const [user, dispatchUser] = useReducer(userReducer, initial);
+  
   //fetch user data
   return (
     <userContext.Provider value={user}>

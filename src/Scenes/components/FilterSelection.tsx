012c3ff616385d7bc7638961c76d5e2ba8e50b39
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2021-01-05 03:16:45 +0330

    add realtime clock to FilterSelection fix key issue in FilterSelection

--- src/Scenes/components/FilterSelection.tsx
+++ src/Scenes/components/FilterSelection.tsx
@@ -1,7 +1,8 @@
 import { Card, Grid, InputAdornment, makeStyles, MenuItem, TextField } from '@material-ui/core';
 import { FilterList } from '@material-ui/icons';
 import { MessageFilters } from 'constants/constants';
-import React from 'react';
+import moment from 'moment-jalaali';
+import React, { useEffect, useRef, useState } from 'react';
 import useMessageFilter, { MessageFilterQuery } from 'services/hooks/useMessageFilter';
 import Spacer from './Spacer';
 
@@ -21,13 +22,22 @@ const FilterSelection : React.FC<{ title: string }> = ({ title }) => {
 
   const [filter, setFilter] = useMessageFilter();
   const allFilters = Object.values(MessageFilters);
+  const [currentTime, setCurrentTime] = useState('');
+  const timerRef = useRef<number>();
+
+  useEffect(() => {
+    timerRef.current = window.setInterval(() => setCurrentTime(moment(new Date()).format('hh:mm:ss')), 1000);
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, [setCurrentTime]);
 
   // fix history issue
   const handleFilterChange = (filterQuery: MessageFilterQuery) => {
     setFilter(filterQuery);
   };
 
-  const currentTime = '۲۰:۳۰:۳۲';
+  // const currentTime = '۲۰:۳۰:۳۲';
 
   return (
     <Grid container>
@@ -56,7 +66,7 @@ const FilterSelection : React.FC<{ title: string }> = ({ title }) => {
         >
           {allFilters.map(({title: filterTitle, query: filterQuery}) => (
             <MenuItem 
-              key={filterQuery} 
+              key={filterQuery || 'ALL'} 
               value={filterTitle} 
               onClick={() => handleFilterChange(filterQuery)}
             >
